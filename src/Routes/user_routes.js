1edd1014df3d891a1e877150d8c05d71891b187e
samay <samayverma44@gmail.com>
2025-09-14 18:23:27 +0530

    Implement user authentication and management features with routes, controllers, and middleware

---
+++ src/Routes/user_routes.js
@@ -0,0 +1,32 @@
+import { Router } from "express";
+import { register } from "../controllers/user_controllers.js";
+// import { upload } from "../middlewares/multer.js";
+import { login } from "../controllers/user_controllers.js";
+import { verifyjwt } from "../middlewares/auth.js";
+import { logout } from "../controllers/user_controllers.js";
+import { refreshaccesstoken,currentuser,password} from "../controllers/user_controllers.js";
+const router=Router();
+
+//http://localhost:8000/api/v1/user/register
+// router.route("/register").post(
+//     upload.fields([
+//     {
+// name:"avatar" ,
+// maxCount:1
+//     },
+    
+// ]),register)
+//http://localhost:8000/api/v1/user/login
+// router.route("/register").post(register)
+router.post("/register", register)
+router.route("/login").post(login)
+
+
+router.route("/logout").post(verifyjwt,logout)
+router.route("/refreshtoken").post(refreshaccesstoken)
+
+router.route("/resetpassword").post(verifyjwt,password)
+router.route("/currentuser").post( currentuser)
+// router.route("/updateavatar").post( upload.single("avatar"),currentuser)
+
+export default router
